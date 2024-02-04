207077c74f1ce169bf9da25a5a6cdfe0a0514741
Sergio Daniel <sergiodanielac9@gmail.com>
2024-02-04 16:34:16 -0600

    Reparamos el bug y finalizamos el programa.

--- suma.py
+++ suma.py
@@ -1,8 +1,11 @@
 #Este programa suma dos numeros: 
+print("\n Este programa suma las 2 listas: \n")
 
 lista = [1,2,3,4,5,6,7,8,9,10]
 lista_2 = [10,9,8,7,6,5,4,3,2,1]
 suma = lambda x, y: x + y 
 
-lista_f = list(map(lista,lista_2, suma))
-print(lista_f)
+#Correjimos el eror al cambiar suma y lista de orden, map primero toma la función.
+lista_f = list(map(suma,lista_2, lista))
+print("\n El resultado de las sumas es: ")
+print(lista_f, "\n")
